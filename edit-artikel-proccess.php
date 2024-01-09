01fbbcae0e47953125e99a5dd3ea064bb7d42399
briant24 <briantadr24@gmail.com>
2024-01-09 22:52:47 +0700

    bug: fix update image

--- edit-artikel-proccess.php
+++ edit-artikel-proccess.php
@@ -6,14 +6,17 @@ $id = $_POST['artikel_id'];
 $judul = $_POST['judul'];
 $konten = $_POST['content'];
 $editorUsername = $_SESSION['username'];
+$upload_dir = "uploads/";
+$uploaded_file = $upload_dir . basename($_FILES["image"]["name"]);
 $querySelectPrevious = "SELECT judul, content FROM articles WHERE id = '" . $id . "'";
 $result = $conn->query($querySelectPrevious);
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
     $previousTitle = $row['judul'];
     $previousContent = $row['content'];
-    $queryUpdate = "UPDATE articles SET judul = '$judul', content = '$konten' WHERE id = $id";
+    $queryUpdate = "UPDATE articles SET judul = '$judul', content = '$konten', image_path = '$uploaded_file' WHERE id = $id";
     if ($conn->query($queryUpdate) === TRUE) {
+        move_uploaded_file($_FILES["image"]["tmp_name"], $uploaded_file);
         $history_query = "INSERT INTO article_history (article_id, editor_username, previous_title, previous_content, actions) VALUES ('$id', '$editorUsername', '$previousTitle', '$previousContent', 'update')";
         $conn->query($history_query);
         echo "<script>alert('Perubahan berhasil disimpan'); window.location = 'index.php'</script>";
